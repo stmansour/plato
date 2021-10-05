9ef596b13ac26035ebe187124ae7decf7cdfef0c
Steve Mansour <sman@stevemansour.com>
2021-10-04 22:39:59 -0700

    WIP: get all rss feeds from nytimes

--- getdata/collect.sh
+++ getdata/collect.sh
@@ -1,11 +1,64 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 
 TMPRSS="tmprss"
 
 declare -a urls=(
-    "https://rss.nytimes.com/services/xml/rss/nyt/US.xml"
-    "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-    "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Africa.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Americas.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/AsiaPacific.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Europe.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/MiddleEast.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/US.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Education.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Upshot.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/NYRegion.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/EnergyEnvironment.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/SmallBusiness.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Economy.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Dealbook.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/MediaandAdvertising.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/YourMoney.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/PersonalTech.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Baseball.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/CollegeBasketball.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/CollegeFootball.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Golf.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Hockey.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/ProBasketball.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/ProFootball.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Soccer.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Tennis.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Science.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Climate.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Space.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Well.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Arts.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/ArtandDesign.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Books.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/SundayBookReview.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Dance.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Movies.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Music.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Television.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Theater.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/FashionandStyle.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/DiningandWine.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/tmagazine.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Jobs.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/RealEstate.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Automobiles.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Lens.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/Obituaries.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/MostEmailed.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/MostShared.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/MostViewed.xml"
+  "https://rss.nytimes.com/services/xml/rss/nyt/sunday-review.xml"
 )
 
 # process  -   process the rss feeds
@@ -14,7 +67,7 @@ declare -a urls=(
 function process() {
     echo "retrieving from: ${1}"
     curl -s "${1}" -o "${TMPRSS}"
-    python3 chomp.py "${TMPRSS}" nytimsrss.csv
+    python3 chomp.py "${TMPRSS}" nytimesrss.csv
 }
 
 # cleanup - remove temp files
